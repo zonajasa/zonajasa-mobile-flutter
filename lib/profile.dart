527ef5a0e37488c856dc603a13de479827b3bf87
M1ft4hul <mift724@gmail.com>
2026-03-04 15:49:36 +0800

    profile Ui

---
+++ lib/profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class profileUi extends StatelessWidget {
+  const profileUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: Text("data")));
+  }
+}
